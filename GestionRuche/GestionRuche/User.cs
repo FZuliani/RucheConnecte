44131fc090c20920369cf6f67d01bcf85c217626
François <zulianifrancois@yahoo.fr>
2018-11-16 14:29:24 +0100

    Initialisation
    Création de la solution et des différentes classes de base

---
+++ GestionRuche/GestionRuche/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionRuche
+{
+    class User
+    {
+
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Passwd { get; set; }
+        public string Name { get; set; }
+        public int Tel { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public bool ActiveBeeK { get; set; }
+
+
+
+
+    }
+}
